dffd5f12cf4be38d15e0331823bc21231c981f1d
whatsasolo <skqhrh45@iit.kw.ac.kr>
2022-02-21 17:35:07 +0900

    수정 및 리뷰

--- CodingTest/stringAndEng.py
+++ CodingTest/stringAndEng.py
@@ -7,3 +7,11 @@ def solution(s):
     answer = int(s)    
     return answer
 print(solution("one4seveneight"))
+
+num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+#정답
+def solution1(s):
+    answer = s
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+    return int(answer)
